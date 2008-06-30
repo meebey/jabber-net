140dcb05784c5f7759aa6278bb5395d68148fb04
Joe Hildebrand <hildjj@gmail.com>
2008-06-30 20:00:39 +0000

    Build 2.1.0.702
    git-svn-id: http://jabber-net.googlecode.com/svn/branches/RELEASE_1_1@703 b87ccc53-a82b-0410-9262-3dc31cef668b

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Cursive Systems, Inc.")]
 [assembly: AssemblyProduct("jabber-net")]
 [assembly: AssemblyCopyright("Copyright (c) Cursive, Inc. 2000-2008")]
-[assembly: AssemblyVersion("2.0.0.613")]
+[assembly: AssemblyVersion("2.1.0.702")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyKeyFile("../../jabbernet.key")]
